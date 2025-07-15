0ff4e00a1ef7323add2efb0cdebbdc48108cf946
”olek11” <>
2025-07-15 16:56:29 +0700

    perbaikan css

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
-<?php  
+<?php
 
 require __DIR__ . "/../public/index.php";
+if (file_exists(__DIR__ . '/../public' . $_SERVER['REQUEST_URI'])) {
+    return false; // Biarkan Vercel menyajikan file statis
+}
+// Logika Laravel lainnya
