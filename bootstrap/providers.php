65df236fd9c9024fd806a39117716c92bddb0a87
”olek11” <>
2025-07-15 10:20:33 +0700

    perubahan pertama

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,18 @@
 <?php
 
-return [
-    App\Providers\AppServiceProvider::class,
-];
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\View\Compilers\CompilerInterface;
+
+class DisableViewCacheProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        // Override compiler to disable compiled views
+        $this->app->extend(CompilerInterface::class, function ($compiler, $app) {
+            $compiler->setCompiledPath(null);
+            return $compiler;
+        });
+    }
+}
